bf9e359e8c5b2a049a3a27a94565fb59e0467f63
choising <chltmdals115@gmail.com>
2020-02-10 13:31:28 +0900

    add Lombok plugin, test lombok

---
+++ src/main/java/io/github/oraindrop/practice/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package io.github.oraindrop.practice.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+
+    private final int amount;
+}
